217b23076500a1aba0faf8b54df11de11ebb1793
“Sadia <Sadia.islam.badhon@gmail.com>
2020-01-05 16:15:02 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,22 @@
 //your code here
+// class Comment extends React.Component {
+//     render () {
+//         return (
+//         <div className="comment">
+//             {this.props.commentText}
+//         </div>
+        
+//         )
+//     }
+// }
+
+
+
+
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+	render() {
+		return <div className="comment">{this.props.commentText}</div>;
+	}
+}
